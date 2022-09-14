c422fd4380d398ca732f87805ee8a4bedeeb2bf2
I-Sammy <samiaislam@outlook.com>
2022-09-15 04:35:25 +1000

    changes in api.js

--- src/api.js
+++ src/api.js
@@ -15,6 +15,21 @@ export const extractLocations = (events) => {
   return locations;
 };
 
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    'https://pu5bfykfqe.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url' + '/' + encodeCode
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
+
+  access_token && localStorage.setItem("access_token", access_token);
+
+  return access_token;
+};
+
 const checkToken = async (accessToken) => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
@@ -23,6 +38,20 @@ const checkToken = async (accessToken) => {
     .catch((error) => error.json());
 
   return result;
+};
+
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
 };
 
 export const getEvents = async () => {
@@ -68,31 +97,5 @@ export const getAccessToken = async () => {
   return accessToken;
 }
 
-const removeQuery = () => {
-  if (window.history.pushState && window.location.pathname) {
-    var newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
-  } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
-  }
-};
 
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const { access_token } = await fetch(
-    'https://pu5bfykfqe.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url' + '/' + encodeCode
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .catch((error) => error);
 
-  access_token && localStorage.setItem("access_token", access_token);
-
-  return access_token;
-};
